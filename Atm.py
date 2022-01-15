7f6e13c6416ea9265780b2a1cb426daceb8beb76
archana472 <74053433+archana472@users.noreply.github.com>
2022-01-15 18:34:40 +0530

    Update Atm.py

--- Atm.py
+++ Atm.py
@@ -4,29 +4,29 @@ class Atm:
         self.pin = pin
 
     def check_balance(self):
-        print("Your balance is 6000")
+        print("Your current balance is 6000")
 
     def withdrawl(self,amount):
         new_amount = 6000 - amount
         print("you have withdrawn amount "+str(amount) +". Your remaining balance is "+ str(new_amount))
 
 def main():
-    Card_number = input("insert your card number:- ")
-    pin_number  = input("enter your pin number:- ")
+    Card_number = input("insert your card number: ")
+    pin_number  = input("enter your pin number: ")
 
     new_user =  Atm(Card_number ,pin_number)
 
-    print("Choose your activity ")
+    print("Choose a number")
     print("1.Balance Enquriy   2.withdrawl")
-    activity = int(input("enter activity number :- "))
+    activity = int(input("enter a number : "))
 
     if (activity == 1):
         new_user.check_balance()
     elif (activity == 2):
-        amount = int(input("enter the amount:- "))
+        amount = int(input("enter the amount: "))
         new_user.withdrawl(amount)
     else:
-        print("enter a valid number")
+        print("please enter valid  number")
 
 
 if __name__ == "__main__":
